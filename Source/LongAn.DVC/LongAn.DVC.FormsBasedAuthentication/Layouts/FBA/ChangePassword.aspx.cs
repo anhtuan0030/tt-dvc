4eaed7d6f6e43e4d7aa7a5539b4af6c8d6d5e7c3
vincentav <vincentav@gmail.com>
2014-09-07 20:49:19 +0000

    Add FBA

---
+++ Source/LongAn.DVC/LongAn.DVC.FormsBasedAuthentication/Layouts/FBA/ChangePassword.aspx.cs
@@ -0,0 +1,17 @@
+using Microsoft.SharePoint.WebControls;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using System.Globalization;
+
+namespace LongAn.DVC.FormsBasedAuthentication
+{
+    /// <summary>
+    /// Code behind for ChangePassword.aspx
+    /// </summary>
+    public partial class ChangePassword : LayoutsPageBase
+    {
+
+    }
+}
